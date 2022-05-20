b351eaa810c7a3e5ee4c48e8b22a0ea6b01537c0
Flavio Rogerio <solebac@hotmail.com>
2022-05-19 23:41:52 -0300

    reload _next

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
+  /*async rewrites() {
     return [
       {
         source: "/:path*",
         destination: "/:path*",
       },
       {
-        source: "/_next/:path*",
+        source: "/:path*",
         destination: "https://credrapidoaraxa.netlify.app/:path*",
       },
     ];
-  },
+  },*/
   async headers() {
     return [
       {
